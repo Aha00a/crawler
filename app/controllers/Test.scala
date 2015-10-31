7cdd0814bddf9a871928576b1b6d6817af9335a2
aha00a <aha00a@gmail.com>
2015-10-31 17:39:06 +0900

    support for people.search.naver.com

--- app/controllers/Test.scala
+++ app/controllers/Test.scala
@@ -78,6 +78,14 @@ class Test @Inject()(implicit cacheApi: CacheApi, system: ActorSystem) extends C
       assertEquals(crawler.description, "ogDescription")
       assertEquals(crawler.image, "ogImage")
     }
+    {
+      val crawler = Crawler.fromUrl("http://people.search.naver.com/search.naver?where=nexearch&sm=tab_ppn&query=%EB%85%B8%EB%AC%B4%ED%98%84&os=99535&ie=utf8&key=PeopleService")
+      assertEquals(crawler.title, "노무현 전 대통령")
+      assertEquals(crawler.description, "출생-사망 1946년 9월 1일, 경상남도 김해 - 2009년 5월 23일 가족 배우자 권양숙, 아들 노건호, 딸 노정연, 형 노건평 관련정보 역대 대한민국 대통령")
+      assertEquals(crawler.image, "http://sstatic.naver.net/people/30/200906061719353321.jpg")
+    }
+
+
     Ok("Ok.")
   }
 
--- app/logics/Crawler.scala
+++ app/logics/Crawler.scala
@@ -4,9 +4,37 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
 class Crawler(document: Document ) {
-  val title = parseOg("title").getOrElse(document.title())
-  val description = parseOg("description").getOrElse(document.select("body").text())
-  val image = parseOg("image").getOrElse("")
+
+
+
+  val title = parseOg("title").orElse(parseTitle()).getOrElse(document.title())
+  val description = parseOg("description").orElse(parseDescription()).getOrElse(document.body().text())
+  val image = parseOg("image").orElse(parseImage()).getOrElse("")
+
+  def parseTitle(): Option[String] = {
+    if(document.baseUri().startsWith("http://people.search.naver.com")) {
+      Some(document.select(".profile_dsc .who").text())
+    } else {
+      None
+    }
+  }
+
+  def parseDescription():Option[String] = {
+    if(document.baseUri().startsWith("http://people.search.naver.com")) {
+      Some(document.select(".profile_dsc .dsc").text())
+    } else {
+      None
+    }
+  }
+
+  def parseImage():Option[String] = {
+    if(document.baseUri().startsWith("http://people.search.naver.com")) {
+      Some(document.select(".profile_wrap .thmb_wrap .thmb .thmb_img").attr("src"))
+    } else {
+      None
+    }
+  }
+
 
   def parseOg(key:String): Option[String] = {
     val contentFromOg = document.select(s"meta[property=og:$key]").attr("content")
