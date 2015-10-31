338377884077da5f3e873b81ae5aea81d3991ca4
aha00a <aha00a@gmail.com>
2015-10-31 17:54:48 +0900

    refactoring

--- app/logics/Crawler.scala
+++ app/logics/Crawler.scala
@@ -7,30 +7,15 @@ class Crawler(document: Document) {
 
 
   val (title, description, image) = (
-    selectOg("title").orElse(selectText(".profile_dsc .who")).getOrElse(document.title()),
-    selectOg("description").orElse(selectText(".profile_dsc .dsc")).getOrElse(document.body().text()),
-    selectOg("image").orElse(selectAttr(".profile_wrap .thmb_wrap .thmb .thmb_img", "src")).getOrElse("")
-    )
+    selectOg("title")       orElse selectText(".profile_dsc .who")                                  getOrElse document.title(),
+    selectOg("description") orElse selectText(".profile_dsc .dsc")                                  getOrElse document.body().text(),
+    selectOg("image")       orElse selectAttr(".profile_wrap .thmb_wrap .thmb .thmb_img", "src")    getOrElse ""
+  )
 
-  def wrapOption(text: String): Option[String] = {
-    if (text != null && text != "")
-      Some(text)
-    else
-      None
-  }
-
-  def selectOg(key: String): Option[String] = {
-    wrapOption(document.select(s"meta[property=og:$key]").attr("content"))
-  }
-
-
-  def selectText(selector: String): Option[String] = {
-    wrapOption(document.select(selector).text())
-  }
-
-  def selectAttr(selector: String, attr: String): Option[String] = {
-    wrapOption(document.select(selector).attr(attr))
-  }
+  def toOption(text: String): Option[String] = if (text != null && text != "") Some(text) else None
+  def selectOg(key: String): Option[String] = toOption(document.select(s"meta[property=og:$key]").attr("content"))
+  def selectText(selector: String): Option[String] = toOption(document.select(selector).text())
+  def selectAttr(selector: String, attr: String): Option[String] = toOption(document.select(selector).attr(attr))
 }
 
 object Crawler {
