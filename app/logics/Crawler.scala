2da163001fd783be3bcc5479dac6fee685e47388
aha00a <aha00a@gmail.com>
2015-10-31 22:06:50 +0900

    add favicon rule

--- app/logics/Crawler.scala
+++ app/logics/Crawler.scala
@@ -10,13 +10,12 @@ class Crawler(document: Document) {
   val (title, description, image) = (
     selectOg("title")       orElse selectText(".profile_dsc .who")                                  getOrElse document.title(),
     selectOg("description") orElse selectText(".profile_dsc .dsc")                                  getOrElse document.body().text(),
-    selectOg("image")       orElse selectAttr(".profile_wrap .thmb_wrap .thmb .thmb_img", "src")    getOrElse ""
+    selectOg("image")       orElse selectAttr(".profile_wrap .thmb_wrap .thmb .thmb_img", "src")    orElse selectAttr("link[rel=shortcut icon]",  "abs:href") getOrElse ""
   )
 
   def selectOg(key: String): Option[String] = document.select(s"meta[property=og:$key]").attr("content").toOption
   def selectText(selector: String): Option[String] = document.select(selector).text().toOption
   def selectAttr(selector: String, attr: String): Option[String] = document.select(selector).attr(attr).toOption
-  
 }
 
 object Crawler {
