e6d41237ea399b20462c12630825d4fc941b7fcf
aha00a <aha00a@gmail.com>
2015-10-31 17:51:31 +0900

    refactoring

--- app/logics/Crawler.scala
+++ app/logics/Crawler.scala
@@ -3,52 +3,38 @@ package logics
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
-class Crawler(document: Document ) {
-
+class Crawler(document: Document) {
 
 
   val (title, description, image) = (
-    parseOg("title").orElse(parseTitle()).getOrElse(document.title()),
-    parseOg("description").orElse(parseDescription()).getOrElse(document.body().text()),
-    parseOg("image").orElse(parseImage()).getOrElse("")
-  )
+    selectOg("title").orElse(selectText(".profile_dsc .who")).getOrElse(document.title()),
+    selectOg("description").orElse(selectText(".profile_dsc .dsc")).getOrElse(document.body().text()),
+    selectOg("image").orElse(selectAttr(".profile_wrap .thmb_wrap .thmb .thmb_img", "src")).getOrElse("")
+    )
 
-  def parseTitle(): Option[String] = {
-    if(document.baseUri().startsWith("http://people.search.naver.com")) {
-      Some(document.select(".profile_dsc .who").text())
-    } else {
+  def wrapOption(text: String): Option[String] = {
+    if (text != null && text != "")
+      Some(text)
+    else
       None
-    }
   }
 
-  def parseDescription():Option[String] = {
-    if(document.baseUri().startsWith("http://people.search.naver.com")) {
-      Some(document.select(".profile_dsc .dsc").text())
-    } else {
-      None
-    }
-  }
-
-  def parseImage():Option[String] = {
-    if(document.baseUri().startsWith("http://people.search.naver.com")) {
-      Some(document.select(".profile_wrap .thmb_wrap .thmb .thmb_img").attr("src"))
-    } else {
-      None
-    }
+  def selectOg(key: String): Option[String] = {
+    wrapOption(document.select(s"meta[property=og:$key]").attr("content"))
   }
 
 
-  def parseOg(key:String): Option[String] = {
-    val contentFromOg = document.select(s"meta[property=og:$key]").attr("content")
-    if (contentFromOg != "") {
-      Some(contentFromOg)
-    } else {
-      None
-    }
+  def selectText(selector: String): Option[String] = {
+    wrapOption(document.select(selector).text())
+  }
+
+  def selectAttr(selector: String, attr: String): Option[String] = {
+    wrapOption(document.select(selector).attr(attr))
   }
 }
 
 object Crawler {
-  def fromUrl(url:String) = new Crawler(Jsoup.connect(url).get())
-  def fromHtml(html:String) = new Crawler(Jsoup.parse(html))
+  def fromUrl(url: String) = new Crawler(Jsoup.connect(url).get())
+
+  def fromHtml(html: String) = new Crawler(Jsoup.parse(html))
 }
